ff72fbbd995e2896ac9d181c9c4b4891a80fe1d8
Hong Yeong Ju <abc123472000@naver.com>
2019-04-08 23:51:50 +0900

    주사위 알고리즘 비선점 완성

--- algorithmForScheduling/algorithmForScheduling.cpp
+++ algorithmForScheduling/algorithmForScheduling.cpp
@@ -10,6 +10,8 @@ SPN 구현 시도
 #include <iostream>
 #include <queue>
 #include <cstring>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 // 프로세스 구조체
@@ -25,6 +27,7 @@ struct Process {
 	int running_time ;			// 돌린시간
 	int remain_time; 				// 실행하기에 남은 시간.
 	int process_arrival_time;		// 프로세서에 들어온 시간.
+	int dice_number;				// 주사위의 크기 
 	Process() :name("null") {};
 	Process(string aname, int aarrival_time, int aburst_time, int awaiting_time, int aturnaround_time, double anormalized_time, int arunning_time, int aremain_time){
 		name = aname;
@@ -36,6 +39,7 @@ struct Process {
 		running_time = arunning_time;
 		remain_time = burst_time;
 		number = Process::count++;
+		dice_number = 0;					// 처음 생성되면 주사위 숫자는 0
 	}
 
 	bool isNull() {
@@ -52,6 +56,14 @@ struct Process {
 			name = "null";
 		}
 	}
+
+	int rolling_dice() {
+		srand((unsigned int)time(NULL));
+		return dice_number = (rand() % 6) + 1;
+	}
+	int rolling_dice(int post_dice) {
+		return dice_number = (rand() % 6) + 1 + post_dice;
+	}
 };
 
 int Process::count = 0;			//프로세스 생성 전 정적 변수 static count 를 추가해서 프로세스를 추가한다. 
@@ -67,6 +79,9 @@ int srtn_process(T* origin_queue, T* table_queue);
 //hrrn 프로세스 함수
 template <typename T>
 int hrrn_process(T* origin_queue, T* table_queue);
+
+template <typename T>
+int dice_process(T* origin_queue, T* table_queue);
 
 // 프로세스 비교 함수,  입력시간으로 정렬하는 함수
 struct comparison {
@@ -98,6 +113,12 @@ struct hrrn {
 	}
 };
 
+struct dice {
+	bool operator()(Process t, Process u) {
+		return t.dice_number > u.dice_number;
+	}
+};
+
 // 키값으로 정렬하는 함수
 struct key {
 	bool operator()(Process t, Process u) {
@@ -126,7 +147,7 @@ int main() {
 	showing(que, table_que);
 
 	printf("----------------- 그래프 출력 결과 -------------\n");
-	printf("총 실행시간 : %d\n", srtn_process(&que, &table_que));			// 큐의 주소를 함수의 매개변수로 사용
+	printf("총 실행시간 : %d\n", dice_process(&que, &table_que));			// 큐의 주소를 함수의 매개변수로 사용
 	
 	showing(que, table_que);
 
@@ -139,7 +160,7 @@ int main() {
 template <typename T>
 int spn_process(T* origin_queue, T* table_queue) {						// table큐는 입력받은 순서대로 테이블을 보여줄 수 있는 큐
 	priority_queue<Process, vector<Process>, spn> memory_queue;		// 메모리 큐
-	priority_queue<Process, vector<Process>, comparison> tmp;
+	priority_queue<Process, vector<Process>, comparison> tmp;		// 총시간을 계산하기 위한 임시 저장 큐.( 어라이벌 타임으로 정렬이 된다.)
 	priority_queue<Process, vector<Process>, key> table_queue_tmp;	// 테이블 큐에 넣기 위한 템프큐
 	Process processor;										// 프로세서 등록
 	Process tmp_process;
@@ -178,6 +199,7 @@ int spn_process(T* origin_queue, T* table_queue) {						// table큐는 입력받은 순
 //				printf("%d 초에 메모리 큐에 %s가 들어왔습니다. \n", t,tmp_process.name.c_str());
 			}
 		}
+
 
 		// processor가 등록이 안되어있을 때는 메모리의 앞에 있는 것을 빼서 등록시킨다.
 		if (processor.isNull()) {
@@ -443,4 +465,146 @@ void showing(T que , T table_queue) {
 		table_queue.pop();
 		printf("프로세스 키 : %d 프로세스 이름 : %s arrive time : %d Burst Time : %d , Waiting time : %d , Turnaround Time : %d , normalized time %.2lf, running time : %d 프로세서 들어온 시간 : %d \n", p.number,p.name.c_str(), p.arrival_time, p.burst_time, p.waiting_time, p.turnaround_time, p.normalized_time, p.running_time, p.process_arrival_time);
 	}
+}
+
+// 주사위 프로세스 ( 우리만의 알고리즘 ) 
+// 주사위 프로세스 각 매 초마다. 주사위를 굴려서 어떤 프로세스가 프로세서를 사용할지 결정한다.
+template <typename T>
+int dice_process(T* origin_queue, T* table_queue) {
+	priority_queue<Process, vector<Process>, dice> memory_queue;		// 메모리 큐
+	priority_queue<Process, vector<Process>, comparison> tmp;  // 총시간을 계산하기 위한 임시 저장 큐.( 어라이벌 타임으로 정렬이 된다.)
+	priority_queue<Process, vector<Process>, key> table_queue_tmp;	// 테이블 큐에 넣기 위한 템프큐
+	priority_queue<Process, vector<Process>, dice> dice_queue;
+	Process processor;										// 프로세서 등록
+	Process tmp_process;
+	int time = 0;
+	int time_burst_sum = 0;
+	int total_time = 0;					// 전체 돌아간 총 시간.
+
+										// 원래 큐를 입력받아서 정렬시키면서 temp 우선순위 큐로 입력시킨다.
+	while (!origin_queue->empty()) {
+		Process temp = origin_queue->front();
+		tmp.push(temp);
+
+		int lastArrival_time = 0;
+		//2 4  0 8
+		if (lastArrival_time < temp.arrival_time)
+			lastArrival_time = temp.arrival_time;
+
+		time_burst_sum += temp.burst_time;
+
+		time = time_burst_sum + lastArrival_time;
+
+		origin_queue->pop();
+	}
+	//printf("time : %d\n", time);
+	// 위쪽은 총 시간이 얼마나 걸리는지를 계산하는 알고리즘이다.
+
+	// time은 t변수 20초까지 카운트 한다.
+	for (int t = 0; t <= time; t++) {
+		// tmp 큐에서 메모리 큐로 넣기
+		int tmp_size = tmp.size();		// tmp 큐의 사이즈
+		for (int i = 0; i < tmp_size; i++) {
+			int arr_time = (int)tmp.top().arrival_time;
+			if (arr_time == t) {
+				tmp_process = tmp.top();
+				tmp_process.rolling_dice();			// 프로세스 들어가기전 주사위를 굴린다.
+				memory_queue.push(tmp_process);		// 메모리 큐에 넣으면 dice 숫자가 가장 큰거 순으로 정렬이 된다.
+				tmp.pop();
+				//				printf("%d 초에 메모리 큐에 %s가 들어왔습니다. \n", t,tmp_process.name.c_str());
+			}
+		}
+
+		// 위의 코드는 arrival time이 t의 시간이 될때마다 memory_queue에 넣는다.
+		int dice_max = 0;
+		while (!memory_queue.empty()) {		// 모든 메모리 큐에 있는 내용을 꺼내봐서 주사위를 굴린 뒤 dice_max 값을 저장한다.
+			Process dice_temp = memory_queue.top();
+			dice_temp.rolling_dice();
+			if (dice_max < dice_temp.dice_number) {
+				dice_max = dice_temp.dice_number;
+			}
+			dice_queue.push(dice_temp);
+			memory_queue.pop();
+		}
+		printf("%d", dice_queue.size());
+
+		int count = 0;
+		do {
+			count = 0;		// dice 큐에 있는 프로세스가 겹칠때마다 카운터를 시킨다.
+			int temp_dice_max = dice_max;
+			while (!dice_queue.empty()) {	// 다이스 큐가 빌때까지
+				Process dice_temp = dice_queue.top();				
+				if ((dice_max != dice_temp.dice_number) || (dice_queue.size() == 1)) {			// 만일 dice_max와 주사위 숫자가 다르면 출력하고자 하는 값이 아니므로.  메모리 큐에 넣는다.
+					memory_queue.push(dice_temp);			// 메모리 큐에 넣는다.
+				    dice_queue.pop();
+				}
+				else {
+					int rolling_temp = dice_temp.rolling_dice(dice_temp.dice_number);
+					if (rolling_temp > temp_dice_max) {					// 최종 주사위 맥스값을 갱신
+						temp_dice_max = rolling_temp;
+					}
+					dice_queue.push(dice_temp);
+					count++;
+					dice_queue.pop();
+				}
+			}
+			dice_max = temp_dice_max;
+		} while (count != 0);		// 만일 카운트가 0 이면 dice_queue에 최종 큰값의 주사위 수가 있으므로.
+		
+
+		// 주사위 굴리는 코드! (만약 주사위의 숫자가 큰것이 겹쳐서 나올 경우를 생각한다.)
+		// processor가 등록이 안되어있을 때는 주사위를 굴려서 가장 큰 숫자가 나온 것을 출력한다. (하지만 겹친경우 또 다시 큰것끼리 주사위를 돌린다.)
+		if (processor.isNull()) {
+			// 메모리큐에서 프로세서로 등록시킨다.
+
+			if (!memory_queue.empty()) {
+				processor = memory_queue.top();
+				memory_queue.pop();
+				processor.running_time = 0;			// 초기화 시킨다.
+				processor.process_arrival_time = t;		// 프로세서에 들어온 시간 기록.
+														//printf("%d 초에 프로세서에 %s가 들어왔습니다. \n", t, processor.name.c_str());			// 문제... 왜 1초씩 땡겨서 들어가지는가?
+			}
+		}
+		if (!processor.isNull()) {
+			// 프로세서에 있는 waiting time보다 메모리 큐의 remain time이 더 작으면 프로세서 큐는 메모리큐에 넣고. 메모리큐는 프로세서에 넣는다.
+			/*
+			if (!memory_queue.empty()) {
+				Process temp_process = memory_queue.top();
+				if (temp_process.remain_time < processor.remain_time) {
+					memory_queue.pop();
+					memory_queue.push(processor);
+					origin_queue->push(processor);
+					//printf("%d초에 프로세스 %s가 메모리 큐에 있는 프로세서 %s와 교환하였습니다.", t, processor.name.c_str(), temp_process.name.c_str());
+					processor = temp_process;
+					processor.process_arrival_time = t;		// 프로세서에 들어온 시간 기록 (체인지 된 시간)
+				}
+			}*/
+
+			if (processor.remain_time > 0) {
+				processor.running_time++;
+				processor.remain_time--;
+			}
+			if (processor.remain_time == 0) {
+				processor.turnaround_time = t - processor.arrival_time + 1;
+				processor.waiting_time = processor.turnaround_time - processor.burst_time;
+				processor.normalized_time = (double)processor.turnaround_time / processor.burst_time;
+				//				printf("%d 초에 프로세서 %s가 origin_queue에 들어왔습니다.\n ", t, processor.name.c_str());
+				origin_queue->push(processor);
+				table_queue_tmp.push(processor);
+				if (t > total_time) {					// 전체 총 돌린 시간이 현재 시간보다 적으면 총 돌린 시간은 지금 시간으로 
+					total_time = t;
+				}
+				processor.setNull(true);
+			}
+
+		}
+	}
+
+	//테이블 큐 에 넣는 작업.
+	while (!table_queue_tmp.empty()) {
+		table_queue->push(table_queue_tmp.top());
+		table_queue_tmp.pop();
+	}
+
+	return total_time + 1;
 }
